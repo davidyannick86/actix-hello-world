fe6196938f4f74d15898f7bc211eb96ec40bcd89
DavidYannick <57354908+davidyannick86@users.noreply.github.com>
2025-01-16 12:01:24 +0100

    Initialize Actix web project with basic routes and HTTP client requests

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("0.0.0.0", 8000))?
+    .run()
+    .await
+}
